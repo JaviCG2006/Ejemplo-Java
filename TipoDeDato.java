acf4a2c2adc7d56423b44340539d3d5599a30959
JaviCG2006 <calado.garcia.javier@iescamas.es>
2025-09-18 19:22:32 +0200

    Add files via upload

---
+++ TipoDeDato.java
@@ -0,0 +1,19 @@
+package hola_mundo;
+
+public class TipoDeDato {
+
+	public static void main(String[] args) {
+		//Este es un tipo de dato numerico
+		int numero = 100;
+		float numero_decimal = 1.1F;
+		double numero_decimal12=1.1;
+		boolean lógico = false;
+		String palabra = "Tomate";
+		char letra = 'd';
+		System.out.println("Numero:" + 100);
+		System.out.println("Numero decimal:" + numero_decimal);
+		System.out.println("Numero:" + lógico);
+
+	}
+
+}
